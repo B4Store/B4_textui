254db4cff4016f2d660140a6d1b5dcf62aad48a8
B4-Store <132667154+B4Store@users.noreply.github.com>
2023-07-08 22:47:59 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# discord.gg\b4s
+```lua
+-- Uses keys from the KEYS table in client
+exports['B4_textui']:showPersist("Get Up", "E", function(result, msg)
+  print(result)
+  print(msg)
+end)
+
+exports['B4_textui']:hidePersist()
+```
---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  MARGIN: '6vh 0 0 0', // (top, right, bottom, left) - use vh or vw units for responsive design
+  BG_COLOR: '', // Background color
+  BOX_SHADOW: '', // Box shadow
+  BORDER_RADIUS: 7, // Border radius
+  TEXT_COLOR: '#ffffff', // Text color
+  KEY_COLOR: '#bd1847', // Key color
+  KEY_BORDER_COLOR: '', // Key border color
+  KEY_BG_COLOR: '', // Key background color
+  X: 'left', // X position (left, center, right)
+  Y: 'center', // Y position (start, center, end)
+  DURATION: 1, // Duration (seconds)
+  EASING: 'power4.inOut', // Easing
+  VOLUME: 0.3, // Volume (0-1)
+}
+
+export default CONFIG;
